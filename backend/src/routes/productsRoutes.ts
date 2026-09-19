869532d66343be37468db1a903aa660e28728127
Halil Ismail <ismailhalil073@gmail.com>
2026-09-19 11:59:27 +0300

    adding and fix

--- backend/src/routes/productsRoutes.ts
+++ backend/src/routes/productsRoutes.ts
@@ -3,9 +3,16 @@ import { AddProducts, DeleteProducts, GetProducts, UpdateProducts } from "../con
 
 const route = express.Router();
 
+// api/products (Get all products)
 route.get("/",GetProducts);
+
+// api/products/add-product (Add a new product)
 route.post("/add-product",AddProducts);
+
+// api/products/update-product/:id (Update a product by ID)
 route.put("/update-product/:id",UpdateProducts);
+
+// api/products/delete-product/:id (Delete a product by ID)
 route.delete("/delete-product/:id",DeleteProducts);
 
-export default route;
+export default route; 
